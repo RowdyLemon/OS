c0bc8315e831732bed3d5f95fe31ec112188139c
Matthew Lemon <matthewlemon08@hotmail.com>
2016-02-11 19:29:47 -0700

    Assignment_2

---
+++ Assignment_2/.hello.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/student/Desktop/Assignment_2/hello.ko := ld -r -m elf_x86_64 -T /usr/src/linux-headers-3.13.0-45-generic/scripts/module-common.lds --build-id  -o /home/student/Desktop/Assignment_2/hello.ko /home/student/Desktop/Assignment_2/hello.o /home/student/Desktop/Assignment_2/hello.mod.o
---
+++ Assignment_2/.tmp_versions/my-module.mod
@@ -0,0 +1,2 @@
+/home/student/Desktop/Assignment_2/my-module.ko
+/home/student/Desktop/Assignment_2/my-module.o
